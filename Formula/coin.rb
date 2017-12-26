41583c50620b800c278bd6f72492aa84e7f94932
Tom Bell <tomb@tomb.io>
2017-12-26 12:46:01 +0000

    Update coin to v0.0.3

--- Formula/coin.rb
+++ Formula/coin.rb
@@ -1,8 +1,8 @@
 class Coin < Formula
   desc "Simple CLI app to get cryptocurrency price information"
   homepage "https://github.com/tombell/coin"
-  url "https://s3.amazonaws.com/coin-builds/coin-v0.0.2.zip"
-  sha256 "12751400238addf591188d35ce016bdb249e8de8cb8a8e464d9ae82d8f034830"
+  url "https://s3.amazonaws.com/coin-builds/coin-v0.0.3.zip"
+  sha256 "53d0a1d8c57cf9b7ce354343ce79ff14516bf66dd8c5c904f552fafec5ff97eb"
 
   def install
     bin.install "coin"
