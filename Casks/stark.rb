46b71e23caa7f3f9a1d0737ef7269cb6c0fe8024
Tom Bell <tomb@tomb.io>
2018-02-12 18:45:25 +0000

    Update stark to 1.0.2

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -1,7 +1,7 @@
 cask 'stark' do
   version '1.0.1'
-  sha256 'e1493bed662ff4dd56cf62f98e736d7258c3132903cc2180311337b70251ca6c'
-  url 'https://s3.amazonaws.com/stark-builds/stark-1.0.1.zip'
+  sha256 '443561d73fbe1ca74699f038ec2afa47c8b4ec388e4b6cbdfc61ad1afde49732'
+  url 'https://s3.amazonaws.com/stark-builds/stark-1.0.2.zip'
 
   name 'Stark'
 
