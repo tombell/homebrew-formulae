5534c35f0d773cde95ec0e921a72f322cb2bfeda
Tom Bell <tomb@tomb.io>
2018-02-12 23:23:36 +0000

    Add formula for prettytest

---
+++ Formula/prettytest.rb
@@ -0,0 +1,13 @@
+class Coin < Formula
+  homepage "https://github.com/tombell/prettytest"
+  url "https://s3.amazonaws.com/prettytest-builds/prettytest-v0.0.1.zip"
+  sha256 "87ee737129100425d21793bb3ae4f8cc873c4fa759f908796c6327fd33cb6e59"
+
+  def install
+    bin.install "prettytest"
+  end
+
+  test do
+    system "#{bin}/prettytest", "-h"
+  end
+end
