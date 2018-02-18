70b063ff95b46bf9f17fdf4f5c30351d29b45c1e
Tom Bell <tomb@tomb.io>
2018-02-18 01:09:43 +0000

    Add formula for releasekit

---
+++ Formula/releasekit.rb
@@ -0,0 +1,14 @@
+class ReleaseKit < Formula
+  desc "Create or update GitHub releases based on closed issues and/or merged pull requests"
+  homepage "https://github.com/tombell/releasekit"
+  url "https://s3.amazonaws.com/releasekit-builds/releasekit-v0.1.1.zip"
+  sha256 "d697bed000182b6a1ac9d7474d494b9ffed84339c6f07c9b99f4bfaf673a3b7b"
+
+  def install
+    bin.install "releasekit"
+  end
+
+  test do
+    system "#{bin}/releasekit", "-h"
+  end
+end
