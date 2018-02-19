cc0194e7aacc3653db60197763a661ff0d59539a
Tom Bell <tomb@tomb.io>
2018-02-19 20:09:24 +0000

    Update stark.rb cask

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -1,5 +1,5 @@
 cask 'stark' do
-  version '1.0.1'
+  version '1.0.2'
   sha256 '443561d73fbe1ca74699f038ec2afa47c8b4ec388e4b6cbdfc61ad1afde49732'
   url 'https://s3.amazonaws.com/stark-builds/stark-1.0.2.zip'
 
@@ -8,8 +8,9 @@ cask 'stark' do
   accessibility_access true
 
   app 'Stark.app'
-  
+
   zap delete: [
+    '~/Library/LaunchAgents/com.rustyrobots.Stark.plist',
     '~/Library/Preferences/co.rustyrobots.Stark.plist',
   ]
 end
