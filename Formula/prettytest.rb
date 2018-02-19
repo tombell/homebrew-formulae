0ab1f44e5aea3c03fcd7c3d4d020bbef05616625
Tom Bell <tomb@tomb.io>
2018-02-19 20:10:38 +0000

    Update prettytest.rb formula

--- Formula/prettytest.rb
+++ Formula/prettytest.rb
@@ -1,5 +1,7 @@
 class Prettytest < Formula
+  desc "Adds color to the output of `go test` on macOS or Linux systems"
   homepage "https://github.com/tombell/prettytest"
+
   url "https://s3.amazonaws.com/prettytest-builds/prettytest-v0.0.1.zip"
   sha256 "87ee737129100425d21793bb3ae4f8cc873c4fa759f908796c6327fd33cb6e59"
 
