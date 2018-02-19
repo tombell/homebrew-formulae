5de07a7266ecc0a00d25a17b10955e6063af0d44
Tom Bell <tomb@tomb.io>
2018-02-19 20:10:07 +0000

    Update coin.rb formula

--- Formula/coin.rb
+++ Formula/coin.rb
@@ -1,6 +1,7 @@
 class Coin < Formula
-  desc "Simple CLI app to get cryptocurrency price information"
+  desc "Command line app to get cryptocurrency price information from CoinMarketCap"
   homepage "https://github.com/tombell/coin"
+
   url "https://s3.amazonaws.com/coin-builds/coin-v0.0.4.zip"
   sha256 "196cea76f3bab2291174a8f125fcea153a7b438fe9f2c1b4b3bf1b5d8422bf34"
 
