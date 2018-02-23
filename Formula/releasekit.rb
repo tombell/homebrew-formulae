b2e60d35deb05d80cee367c9ceb9c6791c23ad09
Tom Bell <tomb@tomb.io>
2018-02-23 19:45:40 +0000

    Update releasekit formula to build from source

--- Formula/releasekit.rb
+++ Formula/releasekit.rb
@@ -1,15 +1,21 @@
 class Releasekit < Formula
-  desc "Create or update GitHub releases based on closed issues and/or merged pull requests"
+  desc "Create GitHub releases from closed issues and pull requests"
   homepage "https://github.com/tombell/releasekit"
-
-  url "https://s3.amazonaws.com/releasekit-builds/releasekit-v0.1.1.zip"
-  sha256 "d697bed000182b6a1ac9d7474d494b9ffed84339c6f07c9b99f4bfaf673a3b7b"
+  url "https://github.com/tombell/releasekit/archive/v0.1.1.tar.gz"
+  sha256 "02fff2cdb0f6f37f749133797d8dfc27a36cf4d65458258ef56a7079ac975f3f"
+  head "https://github.com/tombell/releasekit.git"
 
   def install
-    bin.install "releasekit"
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/tombell/releasekit").install buildpath.children
+    cd "src/github.com/tombell/releasekit" do
+      system "go", "build", "-o", bin/"releasekit", "./cmd/releasekit"
+      prefix.install_metafiles
+    end
   end
 
   test do
-    system "#{bin}/releasekit", "-h"
+    # TODO
+    # system "#{bin}/releasekit"
   end
 end
