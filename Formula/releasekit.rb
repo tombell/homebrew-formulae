f51764f4867bb6adc1995ead6f25cb7da1d7737f
Tom Bell <tomb@tomb.io>
2018-03-31 20:59:31 +0100

    Update releasekit formula

--- Formula/releasekit.rb
+++ Formula/releasekit.rb
@@ -1,9 +1,9 @@
 class Releasekit < Formula
   desc "Create GitHub releases from closed issues and pull requests"
   homepage "https://github.com/tombell/releasekit"
-  url "https://github.com/tombell/releasekit/archive/v0.1.1.tar.gz"
-  sha256 "02fff2cdb0f6f37f749133797d8dfc27a36cf4d65458258ef56a7079ac975f3f"
-  head "https://github.com/tombell/releasekit.git"
+  url "https://github.com/tombell/releasekit.git",
+    :tag => "v0.1.1",
+    :revision => "6c4e8a83b3632c8a5670261c657d8a01c5f0680b"
 
   depends_on "go" => :build
 
@@ -11,7 +11,15 @@ class Releasekit < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/tombell/releasekit").install buildpath.children
     cd "src/github.com/tombell/releasekit" do
-      system "go", "build", "-o", bin/"releasekit", "./cmd/releasekit"
+      commit = Utils.popen_read("git rev-parse --short HEAD").chomp
+      ldflags = [
+        "-X main.Version=#{version}",
+        "-X main.Commit=#{commit}",
+      ]
+      system "go", "build",
+             "-o", bin/"releasekit",
+             "-ldflags", ldflags.join(" "),
+             "github.com/tombell/releasekit/cmd/releasekit"
       prefix.install_metafiles
     end
   end
