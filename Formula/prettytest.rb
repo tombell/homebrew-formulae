277f98cd9126b7ae133a00094128fae985ac1a6d
Tom Bell <tomb@tomb.io>
2018-03-31 20:57:17 +0100

    Update prettytest formula

--- Formula/prettytest.rb
+++ Formula/prettytest.rb
@@ -1,9 +1,9 @@
 class Prettytest < Formula
   desc "Pipe output of `go test` to add color"
   homepage "https://github.com/tombell/prettytest"
-  url "https://github.com/tombell/prettytest/archive/v0.0.2.tar.gz"
-  sha256 "c14dcc718f683b7f4b61b1025ef11fc05aa1b0ceccfc07909ed9465a5cacbed0"
-  head "https://github.com/tombell/prettytest.git"
+  url "https://github.com/tombell/prettytest.git",
+    :tag => "v0.0.2",
+    :revision => "1035e098825e2df85f72b413575feed81f4b4dfb"
 
   depends_on "go" => :build
 
@@ -11,7 +11,14 @@ class Prettytest < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/tombell/prettytest").install buildpath.children
     cd "src/github.com/tombell/prettytest" do
-      system "go", "build", "-o", bin/"prettytest", "./cmd/prettytest"
+      commit = Utils.popen_read("git rev-parse --short HEAD").chomp
+      ldflags = [
+        "-X main.Version=#{version}",
+        "-X main.Commit=#{commit}",
+      ]
+      system "go", "build",
+             "-o", bin/"prettytest",
+             "github.com/tombell/prettytest/cmd/prettytest"
       prefix.install_metafiles
     end
   end
