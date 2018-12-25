7443aca39ae13d33810f95daa8bec33c2e88c93c
Tom Bell <tomb@tomb.io>
2018-12-25 16:14:19 +0000

    Update Formula/moniker.rb to v0.0.6

--- Formula/moniker.rb
+++ Formula/moniker.rb
@@ -2,7 +2,7 @@ class Moniker < Formula
   desc "Command-line app to rename MP3 files based on ID3 tags"
   homepage "https://github.com/tombell/moniker"
   url "https://github.com/tombell/moniker.git",
-    :tag => "v0.0.5",
+    :tag => "v0.0.6",
     :revision => "e750dff744a612ad5bb0f94c801e1ea612803e3f"
 
   depends_on "go" => :build
