8ccf08e1558781f5689ef52d1530ea95c45bdb2f
Tom Bell <tomb@tomb.io>
2019-01-14 04:17:58 +0000

    Update Formula/tldr.rb to v0.1.3

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -2,8 +2,8 @@ class Tldr < Formula
   desc "Simplified and community-driven man pages"
   homepage "https://github.com/tombell/tldr"
   url "https://github.com/tombell/tldr.git",
-    :tag => "v0.1.2",
-    :revision => "5844cb1c23526350ba1872d6ac98c400e1e0b9d4"
+    :tag => "v0.1.3",
+    :revision => "cb8370915eddfe02e518f7f4aea1161a8553c6b8"
 
   depends_on "go" => :build
   depends_on "lodge" => :build
