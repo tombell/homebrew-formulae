dad53088a7615da7fa69a725b3680f39c1884a0e
Tom Bell <tomb@tomb.io>
2019-10-08 21:33:01 +0100

    Update stark cask to 1.3.0

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -1,8 +1,8 @@
 cask 'stark' do
   homepage 'https://github.com/tombell/stark'
-  version '1.2.1'
-  sha256 '33ab9d7f6624cdc84c05ac8ecdcd00efabc40da4fc3bc8009462f2ac3afed6ca'
-  url 'https://s3.amazonaws.com/stark-builds/stark-1.2.1.zip'
+  version '1.3.0'
+  sha256 '181cf108685c55f9b8fb9e4af4d0ab7d83b56e31a76918a2d07f7490deefa261'
+  url 'https://s3.amazonaws.com/stark-builds/stark-1.3.0.zip'
 
   name 'Stark'
 
