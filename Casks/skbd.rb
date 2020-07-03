746e81df720c90182098abf3248dee8ad26586a1
Tom Bell <tomb@tomb.io>
2020-07-03 15:29:13 +0100

    Move skbd to a formula

--- Casks/skbd.rb
+++
@@ -1,8 +0,0 @@
-cask 'skbd' do
-  homepage 'https://github.com/tombell/skbd'
-  version '0.0.1'
-  sha256 '1d3067c0672c08593415fc6b5b2f3107113c09b957b893bde72c0440d7155376'
-  url 'https://skbd-builds.s3.amazonaws.com/skbd-0.0.1.tar.gz'
-
-  binary 'skbd'
-end
---
+++ Formula/skbd.rb
@@ -0,0 +1,54 @@
+class Skbd < Formula
+  desc "Stark Keybind Daemon"
+  homepage "https://github.com/tombell/skbd"
+  url "https://github.com/tombell/skbd.git",
+      :tag => "v0.0.3",
+      :revision => "5131a2085928ec0bfc6b574b6a94d1697aaef8f5"
+  head "https://github.com/tombell/skbd.git"
+
+  depends_on :xcode => :build
+
+  depends_on :macos => :catalina
+
+  def install
+    (var/"log/skbd").mkpath
+    system "make", "package"
+    bin.install "#{buildpath}/.build/release/skbd"
+  end
+
+  plist_options :manual => "skbd"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/skbd</string>
+        </array>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>PATH</key>
+          <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+        </dict>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/skbd/skbd.out.log</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/skbd/skbd.err.log</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    assert_match "skbd version #{version}", shell_output("#{bin}/skbd --version")
+  end
+end
