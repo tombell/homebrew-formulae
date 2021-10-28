17b7e4393d299d5235ddaa222df5f9ea8d2a164d
Tom Bell <tomb@tomb.io>
2021-10-28 21:33:58 +0100

    Update skbd.rb to v0.0.7

--- Formula/skbd.rb
+++ Formula/skbd.rb
@@ -1,17 +1,16 @@
 class Skbd < Formula
   desc "Stark Keybind Daemon"
   homepage "https://github.com/tombell/skbd"
-  url "https://github.com/tombell/skbd.git", tag: "v0.0.6", revision: "92f116dc734b6e5a1f3630b817e6e6e8cae17909"
+  url "https://github.com/tombell/skbd.git", tag: "v0.0.7", revision: "3ac0a5932bcaa9d6081909136cafb429ef2224c7"
   head "https://github.com/tombell/skbd.git", branch: "main"
-
-  depends_on xcode: :build
-  depends_on macos: :monterey
 
   bottle do
     root_url "https://skbd-builds.s3.amazonaws.com"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, monterey: "0e5471ba2d77a4221e0b56659a9f75efed63348d495f1602888b09073f4d7497"
+    sha256 cellar: :any_skip_relocation, monterey: "1336bd6ad4adac92d40fb0a496c9de16505eea3ce022cb2828f5b07142674a5c"
   end
+
+  depends_on xcode: :build
+  depends_on macos: :monterey
 
   def install
     (var/"log/skbd").mkpath
