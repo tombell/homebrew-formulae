7802de7e32f278170bc82ad7094a2ecfc4db31c8
Tom Bell <tomb@tomb.io>
2021-12-22 20:06:17 +0000

    Add doppler cask

---
+++ Casks/doppler.rb
@@ -0,0 +1,9 @@
+cask 'doppler' do
+  homepage 'https://brushedtype.co/doppler/'
+  version '2.0.9'
+  url 'https://updates.brushedtype.co/doppler-macos/download'
+
+  name 'Doppler'
+
+  app 'Doppler.app'
+end
