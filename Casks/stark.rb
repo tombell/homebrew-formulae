57df89318c7031fe122fedfc2abcb1dd951baa1b
Tom Bell <tomb@tomb.io>
2021-12-22 17:24:19 +0000

    Correct shasum algorithm

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -1,7 +1,7 @@
 cask 'stark' do
   homepage 'https://github.com/tombell/stark'
   version '2.1.0'
-  sha256 'ed2284b06f7eb1947bca64ac91647444ad7c593a'
+  sha256 'b156687bf41849d7ab61f434a8564e7b28f81b8a53b22ad2e66057a5a1c3bdb8'
   url 'https://s3.amazonaws.com/stark-builds/stark-2.1.0.zip'
 
   name 'Stark'
