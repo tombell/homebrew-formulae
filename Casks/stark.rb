a8ddaf753c5be3072765a91204bcc30876d4a289
Tom Bell <tomb@tomb.io>
2022-05-07 00:51:23 +0100

    Update stark.rb to 2.1.4

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -1,8 +1,8 @@
 cask 'stark' do
-  version '2.1.3'
-  sha256 '15794dc1dffafe96e2c63ea574caacee99acc97ccb1133aecdd2f83128918929'
+  version '2.1.4'
+  sha256 '81ebf69d93531397614c2db926c0ed1cb1d00c31aea9d53cf878d7e976c88c22'
 
-  url 'https://s3.amazonaws.com/stark-builds/stark-2.1.3.zip'
+  url 'https://s3.amazonaws.com/stark-builds/stark-2.1.4.zip'
   name 'Stark'
   homepage 'https://github.com/tombell/stark'
 
