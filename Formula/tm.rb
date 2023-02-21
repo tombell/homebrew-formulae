010027582e5b86467164f91ffe56e8c6bbb76b29
Tom Bell <tomb@tomb.io>
2023-02-21 01:30:41 +0000

    Update tm to v0.0.2

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,7 +1,7 @@
 class Tm < Formula
   desc "An opinionated manager for tmux sessions"
   homepage "https://github.com/tombell/tm"
-  url "https://github.com/tombell/tm.git", tag: "v0.0.1", revision: "2f6ea950aa2a4151b2b8f54fbbf6d90a513c1f5b"
+  url "https://github.com/tombell/tm.git", tag: "v0.0.2", revision: "2ea5b21e2bcee499d4afa15902d85be25abef071"
   head "https://github.com/tombell/tm.git", branch: "main"
 
   depends_on "go" => :build
