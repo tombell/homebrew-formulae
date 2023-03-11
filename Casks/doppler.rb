5fb56597b6527963c06529a4403f8598049691e8
Tom Bell <tomb@tomb.io>
2023-03-11 14:54:48 +0000

    Remove doppler as its in homebrew casks now

--- Casks/doppler.rb
+++
@@ -1,9 +0,0 @@
-cask 'doppler' do
-  version '2.1.4'
-
-  url 'https://updates.brushedtype.co/doppler-macos/download'
-  name 'Doppler'
-  homepage 'https://brushedtype.co/doppler/'
-
-  app 'Doppler.app'
-end
