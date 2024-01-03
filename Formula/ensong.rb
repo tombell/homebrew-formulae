e90a3db1d07e5abfdbfe5a7ab5ccbb1cd51276fd
Tom Bell <tomb@tomb.io>
2024-01-03 18:18:02 +0000

    Update ensong.rb to v0.3.0

--- Formula/ensong.rb
+++ Formula/ensong.rb
@@ -1,31 +1,31 @@
 class Ensong < Formula
-  desc "Watch directories for mixes and tracklists to upload"
-  homepage "https://github.com/tombell/ensong"
-  url "https://github.com/tombell/ensong.git", tag: "v0.2.0", revision: "1655850a4327b01d2d82ed44f96351545fb23041"
-  head "https://github.com/tombell/ensong.git", branch: "main"
+  desc 'Watch directories for mixes and tracklists to upload'
+  homepage 'https://github.com/tombell/ensong'
+  url 'https://github.com/tombell/ensong.git', tag: 'v0.2.0', revision: '1655850a4327b01d2d82ed44f96351545fb23041'
+  head 'https://github.com/tombell/ensong.git', branch: 'main'
 
-  depends_on "go" => :build
+  depends_on 'go' => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/tombell/ensong").install buildpath.children
-    cd "src/github.com/tombell/ensong" do
+    ENV['GOPATH'] = buildpath
+    (buildpath / 'src/github.com/tombell/ensong').install buildpath.children
+    cd 'src/github.com/tombell/ensong' do
       commit = `git rev-parse HEAD | cut -c -8`.chomp
 
-      system "go", "build",
-             "-o", bin/"ensong",
-             "-ldflags", "-X main.Version=#{version} -X main.Commit=#{commit}",
-             "github.com/tombell/ensong/cmd/ensong"
+      system 'go', 'build',
+             '-o', bin / 'ensong',
+             '-ldflags', "-X main.Version=#{version} -X main.Commit=#{commit}",
+             'github.com/tombell/ensong/cmd/ensong'
 
       prefix.install_metafiles
     end
   end
 
   service do
-    run opt_bin/"ensong"
+    run opt_bin / 'ensong'
     keep_alive true
-    log_path var/"log/ensong.log"
-    error_log_path var/"log/ensong.log"
+    log_path var / 'log/ensong.log'
+    error_log_path var / 'log/ensong.log'
     environment_variables PATH: std_service_path_env
   end
 end
