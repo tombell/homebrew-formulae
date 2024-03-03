7f0a694e2d7dec8056f4d82ade8e50dbb3e0581a
Tom Bell <tomb@tomb.io>
2024-03-03 01:51:05 +0000

    Update migrate formula

--- Formula/migrate.rb
+++ Formula/migrate.rb
@@ -1,24 +1,22 @@
 class Migrate < Formula
-  desc "Migrate is a database migration tool for PostgreSQL and SQLite"
+  desc "Database migration tool for PostgreSQL and SQLite"
   homepage "https://github.com/tombell/migrate"
+
   url "https://github.com/tombell/migrate.git", tag: "v0.0.1-alpha.1", revision: "b9a0c0cf0bb522b690eb24fdd4bc658d6cb3836e"
   head "https://github.com/tombell/migrate.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/tombell/migrate").install buildpath.children
-    cd "src/github.com/tombell/migrate" do
-      commit = `git rev-parse HEAD | cut -c -8`.chomp
+    commit = `git rev-parse HEAD | cut -c -8`.chomp
+    cmds_package = "github.com/tombell/migrate/cmd/migrate/commands"
 
-      system "go", "build",
-             "-o", bin/"migrate",
-            "-ldflags", "-X github.com/tombell/migrate/cmd/migrate/commands.Version=#{version} -X github.com/tombell/migrate/cmd/migrate/commands.Commit=#{commit}",
-             "github.com/tombell/migrate/cmd/migrate"
+    system "go", "build",
+           "-o", bin/"migrate",
+           "-ldflags", "-X #{cmds_package}.Version=#{version} -X #{cmds_package}.Commit=#{commit}",
+           "./cmd/migrate"
 
-      prefix.install_metafiles
-    end
+    prefix.install_metafiles
   end
 
   test do
