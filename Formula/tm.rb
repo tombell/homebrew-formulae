8d126e0c5bb8760827c25487e6f0d4d8340f6aa9
Tom Bell <tomb@tomb.io>
2024-03-03 02:01:15 +0000

    Update tm formula

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,24 +1,21 @@
 class Tm < Formula
-  desc "An opinionated manager for tmux sessions"
+  desc "Opinionated tmux session manager"
   homepage "https://github.com/tombell/tm"
+
   url "https://github.com/tombell/tm.git", tag: "v0.0.2", revision: "2ea5b21e2bcee499d4afa15902d85be25abef071"
   head "https://github.com/tombell/tm.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/tombell/tm").install buildpath.children
-    cd "src/github.com/tombell/tm" do
-      commit = `git rev-parse HEAD | cut -c -8`.chomp
+    commit = `git rev-parse HEAD | cut -c -8`.chomp
 
-      system "go", "build",
-             "-o", bin/"tm",
-             "-ldflags", "-X main.Version=#{version} -X main.Commit=#{commit}",
-             "github.com/tombell/tm/cmd/tm"
+    system "go", "build",
+           "-o", bin/"tm",
+           "-ldflags", "-X main.Version=#{version} -X main.Commit=#{commit}",
+           "./cmd/tm"
 
-      prefix.install_metafiles
-    end
+    prefix.install_metafiles
   end
 
   test do
