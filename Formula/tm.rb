034c87471886ec42f906dc9c597876d4f68f0fe8
Tom Bell <tomb@tomb.io>
2024-10-15 20:42:34 +0100

    Update tm to v0.0.4

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,7 +1,7 @@
 class Tm < Formula
   desc "Opinionated tmux session manager"
   homepage "https://github.com/tombell/tm"
-  url "https://github.com/tombell/tm.git", tag: "v0.0.3", revision: "1c7c37204e33f70d88df01f53860c1693f9b22b6"
+  url "https://github.com/tombell/tm.git", tag: "v0.0.4", revision: "a7f31efe246c587d1dc845fff726167095fd3b6d"
   head "https://github.com/tombell/tm.git", branch: "main"
 
   depends_on "go" => :build
