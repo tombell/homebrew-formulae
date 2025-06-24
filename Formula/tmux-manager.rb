4392df346dfcdab1b94fb4c1467a846e5b720bab
Tom Bell <tomb@tombell.dev>
2025-06-24 09:18:18 +0100

    Rename tm to tmux-manager

--- Formula/tmux-manager.rb
+++ Formula/tmux-manager.rb
@@ -1,4 +1,4 @@
-class Tm < Formula
+class TmuxManager < Formula
   desc "Opinionated tmux session manager"
   homepage "https://github.com/tombell/tm"
   url "https://github.com/tombell/tm.git", tag: "v0.0.4", revision: "a7f31efe246c587d1dc845fff726167095fd3b6d"
