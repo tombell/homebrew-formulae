0fabd645e101e6ea0f26ad170fa12bea4b70ec74
Tom Bell <tomb@tombell.dev>
2026-05-12 19:28:16 +0100

    Update cask and formula files

--- Casks/font-iosevka-custom.rb
+++ Casks/font-iosevka-custom.rb
@@ -1,10 +1,10 @@
-cask "font-iosevka-custom" do
-  version "34.3.0"
-  sha256 "f5bcafa9ad0c0dc31245bac8283430525702b8e566ee5afe7c428747b58c9118"
+cask 'font-iosevka-custom' do
+  version '34.3.0'
+  sha256 'f5bcafa9ad0c0dc31245bac8283430525702b8e566ee5afe7c428747b58c9118'
 
   url "https://tombell-homebrew-assets.s3.us-east-1.amazonaws.com/IosevkaCustom-#{version}.zip"
-  name "Iosevka Custom"
-  homepage "https://github.com/be5invis/Iosevka/"
+  name 'Iosevka Custom'
+  homepage 'https://github.com/be5invis/Iosevka/'
 
-  font "IosevkaCustom.ttc"
+  font 'IosevkaCustom.ttc'
 end
--- Formula/tmux-manager.rb
+++ Formula/tmux-manager.rb
@@ -1,18 +1,19 @@
 class TmuxManager < Formula
-  desc "Opinionated tmux session manager"
-  homepage "https://github.com/tombell/tm"
-  url "https://github.com/tombell/tm.git", tag: "v0.0.5", revision: "f3f38c72e1e71d26b4cf6ffe14ea860108f239f5"
-  head "https://github.com/tombell/tm.git", branch: "main"
+  desc 'Opinionated tmux session manager'
+  homepage 'https://github.com/tombell/tm'
 
-  depends_on "go" => :build
+  url 'https://github.com/tombell/tm/archive/refs/tags/v0.0.5.tar.gz'
+  head 'https://github.com/tombell/tm.git', branch: 'main'
+
+  depends_on 'go' => :build
 
   def install
     commit = `git rev-parse HEAD | cut -c -8`.chomp
 
-    system "go", "build",
-      "-o", bin / "tm",
-      "-ldflags", "-X main.Version=#{version} -X main.Commit=#{commit}",
-      "./cmd/tm"
+    system 'go', 'build',
+           '-o', bin / 'tm',
+           '-ldflags', "-X main.Version=#{version} -X main.Commit=#{commit}",
+           './cmd/tm'
 
     prefix.install_metafiles
   end
