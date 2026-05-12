1bc590275b23093ff3f93d3362e68dc831054d81
Tom Bell <tomb@tombell.dev>
2026-05-12 23:45:55 +0100

    Update custom iosevka to 34.5.0

--- Casks/font-iosevka-custom.rb
+++ Casks/font-iosevka-custom.rb
@@ -1,6 +1,6 @@
 cask 'font-iosevka-custom' do
-  version '34.3.0'
-  sha256 'f5bcafa9ad0c0dc31245bac8283430525702b8e566ee5afe7c428747b58c9118'
+  version '34.5.0'
+  sha256 'c4bdaeaa16351c7080d4b0bcc84cdf11fc90056fba2cef59f69fa00c6ec90ab2'
 
   url "https://tombell-homebrew-assets.s3.us-east-1.amazonaws.com/IosevkaCustom-#{version}.zip"
   name 'Iosevka Custom'
